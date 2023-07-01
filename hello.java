b47e9d6688d500b8856fe6f610535b7a6e5b10c8
RickyTricks <51193568+RickyTricks@users.noreply.github.com>
2023-07-01 15:01:17 +0530

    Update hello.java
    committing to trigger webhook..!

--- hello.java
+++ hello.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 		
 		public String getMessage(boolean bigger) {
 					if (bigger) {
-									return "Hello Universe!";
+									return "Hello Rakesh!";
 											} else {
 															return "Hello World my love!";
 																	}
